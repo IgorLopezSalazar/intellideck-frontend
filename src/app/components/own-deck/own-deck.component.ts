ade58ac9529e89902f3b60b3e888b54349f64937
Igor Lopez <igor.lopezs@alumni.mondragon.edu>
2024-06-23 17:50:28 +0200

    Fix tests

--- src/app/components/own-deck/own-deck.component.ts
+++ src/app/components/own-deck/own-deck.component.ts
@@ -94,7 +94,7 @@ export class OwnDeckComponent {
     this.receivedCardList = this.cardListComponent.cardList;
     if (this.receivedDeck == undefined ||
       this.receivedCardList == undefined) {
-      return ;
+      return;
     }
 
     const getTagsIdPromise = this.getDeckTagsId(this.receivedDeck);
